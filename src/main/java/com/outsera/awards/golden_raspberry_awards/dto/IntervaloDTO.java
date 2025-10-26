dad902c72616c441adc484853efe00aa43346045
Ruvian Souza <ruvian.fabrin@hotmail.com>
2025-10-26 10:52:41 -0300

    feat: Craido os DTOS

---
+++ src/main/java/com/outsera/awards/golden_raspberry_awards/dto/IntervaloDTO.java
@@ -0,0 +1,5 @@
+package com.outsera.awards.golden_raspberry_awards.dto;
+
+public record IntervaloDTO(String producer, int interval, int previousWin,
+        int followingWin) {
+}
---
+++ src/main/java/com/outsera/awards/golden_raspberry_awards/dto/RespostaPremiacaoDTO.java
@@ -0,0 +1,6 @@
+package com.outsera.awards.golden_raspberry_awards.dto;
+
+import java.util.List;
+
+public record RespostaPremiacaoDTO(List<IntervaloDTO> min, List<IntervaloDTO> max) {
+}
